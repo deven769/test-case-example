63a3c11b8696ab0f15d9a6c763c427deddb46c65
deven <devendraa769@gmail.com>
2019-05-24 11:51:57 +0545

    code refactor

--- mysite/posts/views.py
+++ mysite/posts/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.views.generic import ListView
+from .models import Post
+
+
+class PostPageView(ListView):
+    model = Post
+    template_name = 'posts.html'
