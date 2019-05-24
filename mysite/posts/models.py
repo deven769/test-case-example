51007c266f560c1f5f70af1ad9eb1e2630b97438
deven <devendraa769@gmail.com>
2019-05-24 11:56:43 +0545

    code refactor

--- mysite/posts/models.py
+++ mysite/posts/models.py
@@ -9,12 +9,3 @@ class Post(models.Model):
     def __str__(self):
         """A string representation of the model."""
         return self.text
-
-
-from django.views.generic import ListView
-from .models import Post
-
-
-class PostPageView(ListView):
-    model = Post
-    template_name = 'posts.html'
